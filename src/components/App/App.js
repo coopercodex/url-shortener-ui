a2bd59741514662a42525fd1c1b3fbd7c01371a7
Derek Cooper <derekcooper20@gmail.com>
2022-11-15 12:04:48 -0700

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,11 +8,24 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
+      urls: [
+        // {
+        //   id: 1,
+        //   long_url: 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+        //   short_url: 'http://localhost:3001/useshorturl/1',
+        //   title: 'Awesome photo'
+        // }
+      ]
     }
   }
 
   componentDidMount() {
+    getUrls()
+    .then(response => this.setState({urls: response.urls}))
+  }
+
+  addUrl = (newUrl) => {
+    this.setState({urls: [...this.state.urls, newUrl]})
   }
 
   render() {
@@ -20,9 +33,8 @@ export class App extends Component {
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addUrl={this.addUrl} urls={this.state.urls} />
         </header>
-
         <UrlContainer urls={this.state.urls}/>
       </main>
     );
--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -9,21 +9,36 @@ class UrlForm extends Component {
       urlToShorten: ''
     };
   }
-
   handleNameChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+    // this.clearInputs();
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    this.clearInputs();
+    
+    const newUrl = {
+      long_url: this.state.urlToShorten,
+      title: this.state.title
+    }
+    this.props.addUrl(newUrl)
+    // this.clearInputs()
+    fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST', 
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newUrl)
+    })
+    .then(response => response.json())
+    .then(response => this.setState({urls: [...this.props.urls, response]}))
+    // window.location.reload()
   }
 
   clearInputs = () => {
-    this.setState({title: '', urlToShorten: ''});
+    this.setState({ title: '', urlToShorten: '' });
   }
 
   render() {
+    console.log(this.props.urls)
     return (
       <form>
         <input
@@ -37,8 +52,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
           onChange={e => this.handleNameChange(e)}
         />
 
